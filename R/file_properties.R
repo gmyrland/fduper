550c67176660931df664abaa25fc25fc27a0cab9
Glen Myrland <glenmyrland@gmail.com>
2017-11-05 10:16:19 -0500

    Add further file property helper functions for hash generation

--- R/file_properties.R
+++ R/file_properties.R
@@ -30,3 +30,25 @@ get_hash_ <- function(f, algo="md5") {
   digest::digest(file=f, algo=algo)
 }
 get_hash <- Vectorize(get_hash_, "f")
+
+get_dos_ <- function(f, algo="md5", default=get_hash(f)) {
+  if (!file.exists(f)) return(NA)
+  t <- tempfile()
+  on.exit(unlink(t))
+  file.copy(f, t)
+  res <- system2("unix2dos", t, stderr=TRUE, stdout=TRUE)
+  if(grepl("unix2dos: Binary", res[1])) return(default)
+  get_hash_(t, algo)
+}
+get_dos <- Vectorize(get_dos_, c("f", "algo", "default"))
+
+get_unix_ <- function(f, algo="md5", default=get_hash(f)) {
+  if (!file.exists(f)) return(NA)
+  t <- tempfile()
+  on.exit(unlink(t))
+  file.copy(f, t)
+  res <- system2("dos2unix", t, stderr=TRUE, stdout=TRUE)
+  if(grepl("dos2unix: Binary", res[1])) return(default)
+  get_hash_(t, algo)
+}
+get_unix <- Vectorize(get_unix_, c("f", "algo", "default"))
