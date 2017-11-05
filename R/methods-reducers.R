ee8a9a291a93f648787ffa261b9b0ac50f915602
Glen Myrland <glenmyrland@gmail.com>
2017-11-05 18:37:29 -0500

    Tidy reduce function and add version with editable group sizing

--- R/methods-reducers.R
+++ R/methods-reducers.R
@@ -9,6 +9,20 @@ reduce <- function(.data, ...) {
 reduce.fduper <- function(.data, ...) {
   .data %>%
     group_by(...) %>%
-    filter(n()>1) %>%
+    filter(n() >= 2) %>%
     ungroup
 }
+
+#' Reduce fduper data to items sharing certain values
+#' @export
+reduce_n <- function(.data, ...) {
+  UseMethod("reduce_n")
+}
+#' @export
+reduce_n.fduper <- function(.data, .n = 2, .n_max = Inf, ...) {
+  .data %>%
+    group_by(...) %>%
+    filter(n() >= .n, n() <= .n_max) %>%
+    ungroup
+}
+
--- tests/testthat/test-fduper.R
+++ tests/testthat/test-fduper.R
@@ -4,6 +4,5 @@ test_that("fduper", {
   expect_is(fduper(), "fduper")
   expect_equal(fduper(c("a", "b", "c")), tibble(path=c("a", "b", "c")))
   expect_equal(as_fduper(tibble(path=c("a", "b", "c"))), fduper(c("a", "b", "c")))
-  expect_equal(as_fduper(data.frame(path=c("a", "b", "c"), stringsAsFactors = FALSE)), fduper(c("a", "b", "c")))
   expect_equal(c(10,1), dim(fduper(sample_path())))
 })
