02435e6652b093c00dfc06e3e19330b128053613
Glen Myrland <glenmyrland@gmail.com>
2017-11-06 07:44:55 -0500

    Add full bytewise file dupe validation function

--- R/output.R
+++ R/output.R
@@ -1,3 +1,45 @@
+#' Validate
+#' @export
+validate <- function(.data, ...) {
+  UseMethod("validate")
+}
+validate.fduper <- function(.data, chunk_size = 1e6) {
+  read_chunk <- function(con, chunk_size) {
+    readBin(con, integer(), n = chunk_size, size=1)
+  }
+
+  validate_group <- function(paths) {
+    result <- TRUE
+    size <- unique(file.info(paths)$size)
+    if (length(size) > 1)
+      return(false)
+    if (size == 0)
+      return(TRUE)
+    con <- lapply(paths, file, "rb")
+    for (i in seq(from = 0, to = size - 1, by = chunk_size)) {
+      chunks <- lapply(con, read_chunk, chunk_size)
+      cat("Chunks: ", sapply(chunks, digest::digest), sep = "  ", "[", i * chunk_size, "]", "\n")
+      if (!length(unique(chunks)) == 1) {
+        result <- FALSE; break
+      }
+    }
+    lapply(con, close)
+    (result)
+  }
+
+  if (!"group" %in% names(.data)) stop("Use identify(<features>) to apply group ids")
+  .data %>%
+    group_by(group) %>%
+    dplyr::do({
+      if (!validate_group(.$path)) {
+        stop("Non-duplicates found")
+        return(filter(.data, FALSE))
+      }
+      .data
+    }) %>% ungroup
+}
+
+
 #' Write groups file
 #' @export
 to_text_file <- function(.data, file, overwrite=FALSE) {
